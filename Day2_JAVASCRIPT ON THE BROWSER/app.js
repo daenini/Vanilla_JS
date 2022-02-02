32fe5fd824c4fda026e1102a600489f1fb058f1c
Daehyeon Kim <eogus8292@naver.com>
2022-02-03 01:21:31 +0900

    update

--- Day2_JAVASCRIPT ON THE BROWSER/app.js
+++ Day2_JAVASCRIPT ON THE BROWSER/app.js
@@ -21,13 +21,23 @@ const querySelectorAll = document.querySelectorAll(".hello h1");
 const title = document.querySelector("div.hello:first-child h1");
 
 function handleTitleClick(){
+    // const title을 누르면 이벤트 실행
    title.style.color = "blue";
+}
+
+function handleMouseEnter(){
+    // 마우스를 const Title에 올리면 이벤트 실행
+    title.innerText = "Mouse is here!"
+}
+function handleMouseLeave(){
+    title.innerText = "Mouse is gone!";
 }
 
 // event가 발생하면 호출한 function을 전달하는데 그때 ()을 안쓰는게 가장 중요
 // 이벤트가 발생 했을때 function을 실행을 하기 위해
 title.addEventListener("click" , handleTitleClick);
-
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
 
 
 
