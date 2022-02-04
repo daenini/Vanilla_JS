81f07b3fe4353d966a60365b259de75e7a5b8532
Daehyeon Kim <eogus8292@naver.com>
2022-02-04 13:27:17 +0900

    update

--- Day3/app.js
+++ Day3/app.js
@@ -1,18 +1,19 @@
 const loginForm = document.querySelector("#login-form")
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a")
+const greeting = document.querySelector("#greeting");
+const HIDDEN_CLASSNAME = "hidden"; // 일반적으로 string만 포함된 변수는 대문자로 표기함 String을 저장하고 싶을 때 사용
 
 function onLoginsubmit(event){
     event.preventDefault(); // event가 웹에서 발생될때 웹은 새로고침을 해주는데 그것을 막는 함수
-    console.log(loginInput.value);
-   
-}
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const userName = loginInput.value;
+    greeting.innerText = `Hello ${userName}`;
+    // === greeting.innerText = "Hello "+userName;
 
-function handleLinkClick(event){
-    event.preventDefault();
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit" , onLoginsubmit);
-link.addEventListener("click", handleLinkClick)
+
 
 
